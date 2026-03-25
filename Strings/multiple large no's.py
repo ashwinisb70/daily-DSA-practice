6183092c83b3a3b2ca75def953ef7d286ba27789
Ashwini SB <ashwinisb70@gmail.com>
2026-03-25 23:10:05 +0530

    Create multiple large no's.py

---
+++ Strings/multiple large no's.py
@@ -0,0 +1,7 @@
+def multiply_strings(num1, num2):
+    return str(int(num1) * int(num2))
+
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+
+print("Result:", multiply_strings(num1, num2))
